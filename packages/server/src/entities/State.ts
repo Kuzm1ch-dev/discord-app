ab2488a25efe6f534e5e05aa44f8d6bcba3844f5
Kuzm1ch-dev <steam.danya@yandex.ru>
2024-08-12 18:13:51 +0300

    game change

--- packages/server/src/entities/State.ts
+++ packages/server/src/entities/State.ts
@@ -41,7 +41,7 @@ export class State extends Schema {
   createPlayer(sessionId: string, playerOptions: TPlayerOptions) {
     console.log("createPlayer");
     var master = false;
-    console.log(Object.keys(this.players.keys()).length);
+    console.log(this.players.keys.length);
     if (Object.keys(this.players.values()).length == 0){
       console.log("This is master", sessionId)
       master = true
