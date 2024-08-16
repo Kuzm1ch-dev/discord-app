05cebebdcb3539a86ebecf7c914fbba4e66b41fa
Kuzm1ch-dev <steam.danya@yandex.ru>
2024-08-16 20:08:16 +0300

    game export

--- packages/server/src/rooms/StateHandlerRoom.ts
+++ packages/server/src/rooms/StateHandlerRoom.ts
@@ -22,27 +22,32 @@ export class StateHandlerRoom extends Room<State> {
     this.onMessage('ready', (client, _data) => {
       console.log("ready ", client.sessionId)
       this.state.ready(client.sessionId);
+      this.broadcast("messages", `${ client.id } готов!`);
     });
 
     this.onMessage('unready', (client, _data) => {
       console.log("unready ", client.sessionId)
       this.state.unready(client.sessionId);
+      this.broadcast("messages", `${ client.id } не готов!`);
     });
 
     this.onMessage('start', (client, _data) => {
       console.log("start ", client.sessionId)
       this.state.start(client.sessionId);
       this.broadcast("start");
+      this.broadcast("messages", `Игра началась!`);
     });
 
     this.onMessage("spectate", (client, _data) => {
       console.log("spectate ", client.sessionId)
       this.state.spectate(client.sessionId);
+      this.broadcast("messages", `${ client.id } теперь спектатор!`);
     });
 
     this.onMessage("play", (client, _data) => {
       console.log("play ", client.sessionId)
       this.state.play(client.sessionId);
+      this.broadcast("messages", `${ client.id } в игре!`);
     });
   }
 
@@ -53,11 +58,13 @@ export class StateHandlerRoom extends Room<State> {
   onJoin(client: Client, options: TPlayerOptions) {
     console.log("onJoin")
     this.state.createPlayer(client.sessionId, options);
+    this.broadcast("messages", `${ client.id } присоединился к игре!`);
   }
 
   onLeave(client: Client) {
     console.log("onLeave")
     this.state.removePlayer(client.sessionId);
+    this.broadcast("messages", `${ client.id } покинул игру!`);
   }
 
   onDispose() {
