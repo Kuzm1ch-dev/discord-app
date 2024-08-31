54628b75b2c0a86b2ef4f2f4e26a4b93f7ba947d
Kuzm1ch-dev <steam.danya@yandex.ru>
2024-08-31 10:47:15 +0300

    update

--- packages/server/src/rooms/StateHandlerRoom.ts
+++ packages/server/src/rooms/StateHandlerRoom.ts
@@ -1,6 +1,6 @@
-import {Room, Client} from 'colyseus';
-import {TPlayerOptions} from '../entities/Player';
-import {State, IState} from '../entities/State';
+import { Room, Client } from "colyseus";
+import { TPlayerOptions } from "../entities/Player";
+import { State, IState } from "../entities/State";
 
 export class StateHandlerRoom extends Room<State> {
   maxClients = 8;
@@ -9,63 +9,62 @@ export class StateHandlerRoom extends Room<State> {
     this.setState(new State(options));
 
     // Here's where we would add handlers for updating state
-    this.onMessage('startTalking', (client, _data) => {
-      console.log("startTalking")
+    this.onMessage("startTalking", (client, _data) => {
+      console.log("startTalking");
       this.state.startTalking(client.sessionId);
     });
 
-    this.onMessage('stopTalking', (client, _data) => {
-      console.log("stopTalking")
+    this.onMessage("stopTalking", (client, _data) => {
+      console.log("stopTalking");
       this.state.stopTalking(client.sessionId);
     });
 
-    this.onMessage('ready', (client, _data) => {
-      console.log("ready ", client.sessionId)
+    this.onMessage("ready", (client, _data) => {
+      console.log("ready ", client.sessionId);
       this.state.ready(client.sessionId);
-      this.broadcast("messages", `${ client.id } готов!`);
+      this.broadcast("messages", `${client.id} готов!`);
     });
 
-    this.onMessage('unready', (client, _data) => {
-      console.log("unready ", client.sessionId)
+    this.onMessage("unready", (client, _data) => {
+      console.log("unready ", client.sessionId);
       this.state.unready(client.sessionId);
-      this.broadcast("messages", `${ client.id } не готов!`);
+      this.broadcast("messages", `${client.id} не готов!`);
     });
 
     this.onMessage("spectate", (client, _data) => {
-      console.log("spectate", client.sessionId)
-      if (this.state.spectate(client.sessionId)){
-        this.broadcast("messages", `${ client.id } теперь спектатор!`);
+      console.log("spectate", client.sessionId);
+      if (this.state.spectate(client.sessionId)) {
+        this.broadcast("messages", `${client.id} теперь спектатор!`);
       }
     });
 
     this.onMessage("play", (client, _data) => {
-      console.log("play", client.sessionId)
-      if(this.state.play(client.sessionId)){
-        this.broadcast("messages", `${ client.id } в игре!`);
+      console.log("play", client.sessionId);
+      if (this.state.play(client.sessionId)) {
+        this.broadcast("messages", `${client.id} в игре!`);
       }
     });
 
-    this.onMessage('start', (client, _data) => {
-      console.log("start", client.sessionId)
-      if (this.state.start(client.sessionId)){
+    this.onMessage("start", (client, _data) => {
+      console.log("start", client.sessionId);
+      if (this.state.start(client.sessionId)) {
         this.broadcast("start");
         this.broadcast("messages", `Игра началась!`);
-        this.turn()
+        this.turn();
       }
     });
 
     this.onMessage("shot", (client, _data) => {
       console.log(_data);
-      console.log(`${client.sessionId} shot to ${_data.targetId}`)
-      if (this.state.shot()){
-        this.state.kill(_data.targetId)
-        console.log(`${client.sessionId} kill ${_data.targetId}`)
-        this.broadcast("messages", `${_data.targetId} был убит!`)
+      console.log(`${client.sessionId} shot to ${_data.targetId}`);
+      if (this.state.shot()) {
+        this.state.kill(_data.targetId);
+        console.log(`${client.sessionId} kill ${_data.targetId}`);
+        this.broadcast("messages", `${_data.targetId} был убит!`);
       }
-      if (this.state.aliveCount() > 1){
-
+      if (this.state.aliveCount() > 1) {
       }
-      this.turn()
+      this.turn();
     });
   }
 
@@ -74,39 +73,39 @@ export class StateHandlerRoom extends Room<State> {
   }
 
   onJoin(client: Client, options: TPlayerOptions) {
-    console.log("onJoin")
+    console.log("onJoin");
     this.state.createPlayer(client.sessionId, options);
-    this.broadcast("messages", `${ client.id } присоединился к игре!`);
+    this.broadcast("messages", `${client.id} присоединился к игре!`);
   }
 
   onLeave(client: Client) {
-    console.log("onLeave")
+    console.log("onLeave");
     this.state.removePlayer(client.sessionId);
-    this.broadcast("messages", `${ client.id } покинул игру!`);
+    this.broadcast("messages", `${client.id} покинул игру!`);
   }
 
   onDispose() {
-    console.log('Dispose StateHandlerRoom');
+    console.log("Dispose StateHandlerRoom");
   }
 
-  turn(){
-    console.log("Turn!")
+  turn() {
+    console.log("Turn!");
     var nextUserId = this.state.next();
-    if (nextUserId !== false && typeof(nextUserId) == "string"){
+    if (nextUserId !== false && typeof nextUserId == "string") {
       var sessionId = this.state.players.get(nextUserId)?.sessionId;
-      console.log(`turn ${sessionId}`)
-      if(sessionId){
+      console.log(`turn ${sessionId}`);
+      if (sessionId) {
         var client = this.clients.getById(sessionId);
-        if(client){
+        if (client) {
           client.send("turn");
           client.send("messages", "Ваш ход!");
         }
       }
-    }else{
-      console.log("New Round!")
-      if (this.state.newRound()){
+    } else {
+      console.log("New Round!");
+      if (this.state.newRound()) {
         this.turn();
-      }else{
+      } else {
         this.broadcast("messages", `Все мертвы, игра окончена!`);
       }
     }
